a59f69469ec9f758563c4c4d75516323c32e4e14
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2025-12-30 11:29:37 +0500

    init

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 // @title        Rest-Template (API)
 // @version      1.0
-// @description  <b>Описание:</b> Сервис доставки.<br/><b>Ответственная команда:</b> Checkout Team;<br/><b>Git:</b> <a href="https://gitlab.almanit.kz/jmart/shipping-service-go">https://gitlab.almanit.kz/jmart/shipping-service-go</a>;<br/><b>Golang SDK:</b> <a href="https://gitlab.almanit.kz/jmart/gosdk">https://gitlab.almanit.kz/jmart/gosdk</a>;<br/><b>Документация:</b> <a href="https://conf.almanit.kz/display/CHE/shipping-service-go">https://conf.almanit.kz/display/CHE/shipping-service-go</a>;<br/><b>Readme file:</b> <a href="https://gitlab.almanit.kz/jmart/shipping-service-go/-/blob/master/README.md">README.md</a>
+// @description  <b>Описание:</b> Пример приложения
 // @host         0.0.0.0:8090
 // @BasePath     /
 func main() {
